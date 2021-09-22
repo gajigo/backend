e7aa97fcaa363632f1544825b59b94369187a11a
Pedro Henrique Garcia <pehdfms@gmail.com>
2021-09-22 19:40:25 -0300

    add class Avaliacao

---
+++ src/Models/Avaliacao.java
@@ -0,0 +1,47 @@
+package Models;
+
+public class Avaliacao {
+    private float nota;
+    private Palestra palestraAssociada;
+    private Usuario avaliador;
+    private boolean ativo;
+
+    public Avaliacao(float nota, Palestra palestraAssociada, Usuario avaliador) {
+        this.nota = nota;
+        this.palestraAssociada = palestraAssociada;
+        this.avaliador = avaliador;
+        this.ativo = true;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    public Palestra getPalestraAssociada() {
+        return palestraAssociada;
+    }
+
+    public void setPalestraAssociada(Palestra palestraAssociada) {
+        this.palestraAssociada = palestraAssociada;
+    }
+
+    public Usuario getAvaliador() {
+        return avaliador;
+    }
+
+    public void setAvaliador(Usuario avaliador) {
+        this.avaliador = avaliador;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
