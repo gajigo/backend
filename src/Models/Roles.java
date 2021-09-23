0182ad71fa023a3ac39f33bb7e6898eea98b0962
Pedro Henrique Garcia <pehdfms@gmail.com>
2021-09-23 16:52:32 -0300

    feat: adiciona roles para sistema de permissoes

---
+++ src/Models/Roles.java
@@ -0,0 +1,8 @@
+package Models;
+
+public enum Roles {
+    ORGANIZADOR,
+    ADMIN,
+    PALESTRANTE,
+    CLIENTE
+}
--- src/Models/Usuario.java
+++ src/Models/Usuario.java
@@ -1,29 +1,50 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Usuario {
     private int userId;
     private String nome;
     private String senha;
     private String statusLogin;
+    private ArrayList<Roles> roles;
 
-    public Usuario() {
+    public static void main(String[] args) {
+        Usuario usuarioTeste = new Usuario("Fabiano", "dQw4w9WgXcQ");
+
+        // Testa que cargos iniciam vazios
+        System.out.println(usuarioTeste.roles);
+
+        // Testa adicionar novos cargos
+        usuarioTeste.addRole(Roles.ORGANIZADOR);
+        usuarioTeste.addRole(Roles.PALESTRANTE);
+
+        System.out.println(usuarioTeste.roles);
+
+        // Testa se ignoramos cargos duplicados
+        usuarioTeste.addRole(Roles.ORGANIZADOR);
+
+        System.out.println(usuarioTeste.roles);
     }
 
-    public Usuario(int userId, String nome, String senha, String statusLogin) {
+    public Usuario() {
+        this.roles = new ArrayList<>();
+    }
+
+    public Usuario(int userId, String nome, String senha, String statusLogin, ArrayList<Roles> roles) {
         this.userId = userId;
         this.nome = nome;
         this.senha = senha;
         this.statusLogin = statusLogin;
+        this.roles = roles;
     }
 
-    @Override
-    public String toString() {
-        return "Usuario{" +
-                "userId=" + userId +
-                ", nome='" + nome + '\'' +
-                ", senha='" + senha + '\'' +
-                ", statusLogin='" + statusLogin + '\'' +
-                '}';
+    public Usuario(String nome, String senha) {
+        this.userId = 0;
+        this.nome = nome;
+        this.senha = senha;
+        this.statusLogin = "logado";
+        this.roles = new ArrayList<>();
     }
 
     public int getUserId() {
@@ -56,6 +77,25 @@ public class Usuario {
 
     public void setStatusLogin(String statusLogin) {
         this.statusLogin = statusLogin;
+    }
+
+    public ArrayList<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<Roles> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(Roles novoRole) {
+        if (this.roles.contains(novoRole)) {
+            return;
+        }
+        this.roles.add(novoRole);
+    }
+
+    public void removeRole(Roles role) {
+        this.roles.remove(role);
     }
 
     public boolean verificaLogin(String senhaTeste) {
