8a67e779609dad701c65b3089de81b5024874d80
Pedro Henrique Garcia <pehdfms@gmail.com>
2021-09-22 22:11:28 -0300

    feat: implementa classe Organizador

--- src/Models/Organizador.java
+++ src/Models/Organizador.java
@@ -1,4 +1,36 @@
 package Models;
+import java.util.ArrayList;
 
 public class Organizador {
+    private ArrayList<Eventos> eventosAssociados;
+
+    public Organizador() {
+    }
+
+    public Organizador(ArrayList<Eventos> eventosAssociados) {
+        this.eventosAssociados = eventosAssociados;
+    }
+
+    @Override
+    public String toString() {
+        return "Organizador{" +
+                "eventosAssociados=" + eventosAssociados +
+                '}';
+    }
+
+    public ArrayList<Eventos> getEventosAssociados() {
+        return eventosAssociados;
+    }
+
+    public void setEventosAssociados(ArrayList<Eventos> eventosAssociados) {
+        this.eventosAssociados = eventosAssociados;
+    }
+
+    public void addEventoAssociado(Eventos novoEvento) {
+        this.eventosAssociados.add(novoEvento);
+    }
+
+    public void removeEventoAssociado(Eventos evento) {
+        this.eventosAssociados.remove(evento);
+    }
 }
