27591c02984d70e7d7e2a776e10bae6a87907f9a
Pedro Henrique Garcia <pehdfms@gmail.com>
2021-10-20 21:41:28 -0300

    feat: adiciona abstractDao

---
+++ src/DAO/AbstractDAO.java
@@ -0,0 +1,52 @@
+package DAO;
+
+import java.io.*;
+import java.util.List;
+
+public class AbstractDAO<T> {
+    private String filename;
+
+    public AbstractDAO(String filename) {
+        this.filename = filename;
+    }
+
+    public void save(List<T> models) {
+        try {
+            FileOutputStream file = new FileOutputStream(filename);
+            ObjectOutputStream outfile = new ObjectOutputStream(file);
+            outfile.writeObject(models);
+            outfile.flush();
+            outfile.close();
+            file.close();
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel salvar usuario");
+        }
+    }
+
+    public List<T> load() {
+        try {
+            FileInputStream file = new FileInputStream(filename);
+            ObjectInputStream infile = new ObjectInputStream(file);
+
+            // FODA SE
+            @SuppressWarnings("unchecked")
+            List<T> models = (List<T>) infile.readObject();
+
+            infile.close();
+            file.close();
+
+            return models;
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel carregar usuarios");
+            return null;
+        }
+    }
+
+    public void clean() {
+        File file = new File(filename);
+
+        if (!file.delete()) {
+            System.out.println("Erro ao limpar arquivo");
+        }
+    }
+}
