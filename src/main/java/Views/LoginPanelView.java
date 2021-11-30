2b77a44c10ca58d999190468b5df09c1b4248a07
Helcio L. do Carmo Junior <helcio@carmo.in>
2021-11-30 18:23:28 -0300

    feat: JFrame para login no sistema

---
+++ src/main/java/Views/LoginPanelView.java
@@ -0,0 +1,17 @@
+package Views;
+
+import javax.swing.*;
+
+public class LoginPanelView {
+    private JPanel loginPanel;
+    private JTextField userNameField;
+    private JButton submitLogin;
+    private JPanel formPanel;
+    private JLabel passwordLabel;
+    private JTextField passwordField;
+    private JLabel userNameLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
